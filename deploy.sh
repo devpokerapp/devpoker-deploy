ceb72eb508acab30c6e37f137995a697ffaebdc4
Arthur <rthurmedeiros@gmail.com>
2024-03-12 22:58:16 -0300

    Added registry variable to deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@
 
 AWS_ACCOUNT_ID=810663594687
 AWS_REGION=us-east-2
+REGISTRY_URL=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
